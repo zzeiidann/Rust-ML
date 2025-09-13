533e7336fedc7e3b35461bc369c2f20d9d04a74a
M Raffy Zeidan <mraffyzeidan@Ms-MacBook-Air.local>
2025-09-13 22:24:06 +0700

    Fix: turn RNN into normal folder

---
+++ RNN/target/debug/RNN-rust.d
@@ -0,0 +1,1 @@
+/Users/mraffyzeidan/Downloads/Rust-ML/RNN/target/debug/RNN-rust: /Users/mraffyzeidan/Downloads/Rust-ML/RNN/src/main.rs
---
+++ RNN/target/debug/deps/unicode_ident-431ef9dc7d67baf7.d
@@ -0,0 +1,8 @@
+/Users/mraffyzeidan/Documents/Rust-ML/target/debug/deps/unicode_ident-431ef9dc7d67baf7.d: /Users/mraffyzeidan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/mraffyzeidan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
+
+/Users/mraffyzeidan/Documents/Rust-ML/target/debug/deps/libunicode_ident-431ef9dc7d67baf7.rlib: /Users/mraffyzeidan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/mraffyzeidan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
+
+/Users/mraffyzeidan/Documents/Rust-ML/target/debug/deps/libunicode_ident-431ef9dc7d67baf7.rmeta: /Users/mraffyzeidan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs /Users/mraffyzeidan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs
+
+/Users/mraffyzeidan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/lib.rs:
+/Users/mraffyzeidan/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/unicode-ident-1.0.18/src/tables.rs:
